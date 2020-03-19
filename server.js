ac18138b5553d9458b765190e6fa26be0df6272f
Michael Padiernos <mike.padiernos@gmail.com>
2020-03-18 22:27:53 -0400

    [REFACTOR] Server / Helmet & Middlewares
    - added Helmet & imported Middlewares

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 // IMPORT MODULES
 const express         = require('express');
 const cors            = require('cors');
+const helmet          = require('helmet');
+
+// IMPORT MIDDLEWARES
+const middle          = require('./middleware/middleware');
+
+// MIDDLEWARES
+const logger          = middle.logger();
 
 // IMPORT ROUTERS
 const users           = require('./users/userRouter');
+const posts           = require('./posts/postRouter');
 
 // INITIATE EXPRESS AS SERVER
 const server          = express();
 
-server.use(express.json());
+server.use(helmet(), logger, express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+server
+  .route('/')
+  .get( (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
+  });
 
-// ROUTER = "./api/users"
+// ROUTER - "./api/users"
 server.use('/api/users', users);
 server.use('/users', users);
 
-//custom middleware
-
-function logger(req, res, next) {}
+// ROUTER - "./api/posts"
+server.use('/api/posts', posts);
+server.use('/posts', posts);
 
 module.exports = server;
